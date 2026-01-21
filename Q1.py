3cf059f8413dc9d1ee3c6449ff702b220bbafa31
AryanxAneja <aryan.29630@stu.upes.ac.in>
2026-01-21 20:19:51 +0530

    Day 1

---
+++ Q1.py
@@ -0,0 +1,6 @@
+# Write a program to input two numbers and print their sum
+a = int(input("Enter the first Number: "))
+b = int(input("Enter the second Number: "))
+# sum = a+b    this method can also be used
+# print(sum)
+print("The sum of the two Numbers is : ",a+b)
---
+++ Q3.py
@@ -0,0 +1,4 @@
+# Write a program to input 2 floating points and print their average
+p1 = float(input("Enter 1st Num:"))
+p2 = float(input("Enter 2nd Num:"))
+print("Average is:",(p1+p2)/2)
