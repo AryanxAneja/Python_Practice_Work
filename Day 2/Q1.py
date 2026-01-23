5bc8825f9b785f04a30eac824a3e14c33730816c
AryanxAneja <aryan.29630@stu.upes.ac.in>
2026-01-23 23:56:17 +0530

    Day 2 and Day 3

---
+++ Day 2/Q1.py
@@ -0,0 +1,3 @@
+# wap tp input users first name and print its length
+Name = input("Enter your first name:")
+print("Length of the name is:",len(Name))
---
+++ Day 3/Q1.py
@@ -0,0 +1,3 @@
+# Wap to find the occurence of $ in a string.
+Test_str = "H$ WH$T IS $P G$YS"
+print(Test_str.count("$"))
