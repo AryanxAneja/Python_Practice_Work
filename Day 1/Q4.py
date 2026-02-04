f8d17ce5a611c08f2176d571779700b9ae366ed5
AryanxAneja <aryan.29630@stu.upes.ac.in>
2026-02-04 10:52:49 +0530

    Delete Day 1 directory

--- Day 1/Q4.py
+++
@@ -1,5 +0,0 @@
-# Write a program to input two integers a and b, print true if a is greater than or equall to b. If not print false.
-a = int(input("Enter a Number:"))
-b = int(input("Enter another Number:"))
-print(a>=b)
-# Can also use if else condition
