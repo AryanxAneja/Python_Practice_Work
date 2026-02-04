6193f6ad1441899f56a2248364c64041c4834f45
AryanxAneja <aryan.29630@stu.upes.ac.in>
2026-02-04 10:54:35 +0530

    Delete Day 8 directory

--- Day 8/Q1.py
+++
@@ -1,7 +0,0 @@
-# Swap two numbers without additional value
-a = int(input("Enter first Number:"))
-b = int(input("Enter Second Number:"))
-a = a+b
-b = a-b
-a = a-b
-print("First Number is : ",a,"Second number is:",b)
